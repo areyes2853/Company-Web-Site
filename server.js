d019bb495094cfb4b0195f850c5cf113a9146dd5
areyes2853 <46231827+areyes2853@users.noreply.github.com>
2025-03-19 15:17:29 -0400

    adding a nav

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-const pastwork = [{Work1: "Flower contest", Role: "Judge", Work2: "Halloween show", Role: "Pumpkin suppliers", Work3: "Gas station flower sellers", Role: "We needed money"}];
-const staff = [{EmployeeName: "John", Role: "Manager", EmployeeName: "Sally", Role: "Flower keeper", EmployeeName: "Mike", Role: "Monkey"}];
+const pastwork = [{Work1: "Flower contest", Role: "Judge"}, {Work2: "Halloween show", Role: "Pumpkin suppliers"}, {Work3: "Gas station flower sellers", Role: "We needed money"}];
+const staff = [{EmployeeName: "John", Role: "Manager"}, {EmployeeName: "Sally", Role: "Flower keeper"}, {EmployeeName: "Mike", Role: "Monkey"}];
 const externalLinks = [{LinkTitle: "https://harry'sflowersandotherstuff.com"}];
-
+let message = `We started in 1900 with just a little seed, and now we service all the plant companies in the world!`
 
 //starter page
 app.get('/home',(req,res)=>{
-    res.render('home.ejs')});
+    res.render('home.ejs',{welcome:message,staff:staff,pastwork:pastwork});});
     
 
 app.get('/server', (req, res) => {
